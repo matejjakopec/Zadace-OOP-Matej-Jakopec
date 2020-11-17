3561e08c4895cb8874e92ddfc61bf26ed4d6390d
matejjakopec <73431727+matejjakopec@users.noreply.github.com>
2020-11-17 22:40:55 +0100

    Add files via upload

---
+++ DZ2/PrviOOP/Episode.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrviOOP
+{
+    class Episode
+    {
+        private int viewers;
+        private double totalSum, highestScore;
+        Description description;
+        public Episode (int viewers, double totalSum, double highestScore, Description description)
+        {
+            this.viewers = viewers;
+            this.totalSum = totalSum;
+            this.highestScore = highestScore;
+            this.description = description;
+        }
+        public Episode(int viewers, double totalSum, double highestScore)
+        {
+            this.viewers = viewers;
+            this.totalSum = totalSum;
+            this.highestScore = highestScore;
+        }
+
+        public override string ToString()
+        {
+            return viewers + " " + totalSum + " " + highestScore + " " + description.GetNumber() + " " + description.GetLength() + " " + description.GetName() ;
+        }
+
+        public Episode()
+        {
+            viewers = 0;
+            totalSum = 0.0;
+            highestScore = 0.0;
+        }
+        public void SetMaxScore(double highestScore)
+        {
+            this.highestScore = highestScore;
+        }
+
+        public void SetTotalSum(double totalSum)
+        {
+            this.totalSum = totalSum;
+        }
+        public void SetViewerCount(int viewers)
+        {
+            this.viewers = viewers;
+        }
+        public double GetMaxScore()
+        {
+            return highestScore;
+        }
+
+        public double GetTotalSum()
+        {
+            return totalSum;
+        }
+
+        public double GetAverageScore()
+        {
+            return totalSum / viewers;
+        }
+
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+
+        public void AddView(double score)
+        {
+            this.totalSum += score;
+            this.viewers++;
+            if (this.highestScore<score)
+            {
+                this.highestScore = score;
+            }
+        }
+
+        public double GenerateRandomScore()
+        {
+            Random random = new Random();
+            return random.NextDouble() * (10.0 - 0.0) + 0.0;
+        }
+
+        public static bool operator <(Episode lhs, Episode rhs)
+        {
+            if (lhs is null) return rhs is null;
+            if (lhs.GetAverageScore() < rhs.GetAverageScore())
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >(Episode lhs, Episode rhs)
+        {
+            if (lhs is null) return rhs is null;
+            if (lhs.GetAverageScore() > rhs.GetAverageScore())
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
---
+++ DZ2/PrviOOP/TvUtilities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrviOOP
+{
+    class TvUtilities
+    {
+        public double GenerateRandomScore()
+        {
+            Random random = new Random();
+            return random.NextDouble() * (10.0 - 0.0) + 0.0;
+        }
+
+        static public Episode Parse(string fileInput)
+        {
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+            string[] words = fileInput.Split(',');
+            int viewers = int.Parse(words[0]);
+            double totalSum = Convert.ToDouble(words[1],provider);
+            double maxScore = Convert.ToDouble(words[2],provider);
+            int number = int.Parse(words[3]);
+            TimeSpan length = new TimeSpan();
+            length = TimeSpan.Parse(words[4]);
+            string name = words[5];
+            Description description = new Description(number, length, name);
+            Episode episode = new Episode(viewers, totalSum, maxScore, description);
+            return episode;
+        }
+         static public void Sort(Episode[] episodes)
+        {
+            for (int i = 0; i < episodes.Length - 1; i++)
+            {
+                for (int j = 0; j < episodes.Length - 1; j++)
+                {
+                        if (episodes[i] > episodes[j]) 
+                        {
+                            Episode helper = episodes[i];
+                            episodes[i] = episodes[j];
+                            episodes[j] = helper;
+                        }
+                }
+            }
+        }
+    }
+}
